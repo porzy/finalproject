d42c0ff712442af18c3be493766b64017e8bbe64
tporznak <porznak@gmail.com>
2019-11-10 14:34:44 -0600

    added orders model

---
+++ db/migrate/20191110203433_create_orders.rb
@@ -0,0 +1,13 @@
+class CreateOrders < ActiveRecord::Migration[6.0]
+  def change
+    create_table :orders do |t|
+      t.string :status
+      t.reference :user
+      t.decimal :subtotal
+      t.decimal :tax
+      t.decimal :total_price
+
+      t.timestamps
+    end
+  end
+end
