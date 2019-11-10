0a6c152c215eb8c2edd658d363e952d7d28ab0ec
tporznak <porznak@gmail.com>
2019-11-10 14:39:50 -0600

    db migrate

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,14 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+  helper_method :current_order
+
+  def current_order
+    if session[:order_id]
+      Order.find(session[:order_id])
+    else
+      Order.new
+    end
+  end
 end
